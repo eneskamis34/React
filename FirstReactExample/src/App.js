46db6ffe5ec9ebe1080b8ebb327bc2c2feaf0a98
Enes Kamis <eneskamis@yandex.com>
2020-11-13 14:08:51 +0300

    ReactStrap and Props
    ReactStrap and Props

--- FirstReactExample/src/App.js
+++ FirstReactExample/src/App.js
@@ -1,13 +1,26 @@
 import Navi from './Navi';
 import CategoryList from './CategoryList';
 import ProductList from './ProductList';
+import {Container, Row, Col} from 'reactstrap';
 
 function App() {
+  let productInfo = {title:"Product List"}
+  let categoryInfo = {title:"Category List"}
   return (
     <div>
-      <Navi/>
-      <CategoryList/>
-      <ProductList/>
+      <Container>
+        <Row>
+          <Navi/>
+        </Row>
+        <Row>
+          <Col xs="3">
+          <CategoryList info={categoryInfo}/>
+          </Col>
+          <Col xs="9">
+          <ProductList info={productInfo}/>
+          </Col>
+        </Row>
+        </Container>
     </div>
   );
 }
