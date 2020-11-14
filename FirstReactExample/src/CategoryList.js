e11315f75c12371308962070fccf8a680477b1a5
Enes Kamis <eneskamis@yandex.com>
2020-11-14 14:06:41 +0300

    State - Event - Components
    State - Event - Components

--- FirstReactExample/src/CategoryList.js
+++ FirstReactExample/src/CategoryList.js
@@ -1,18 +1,38 @@
 import React, { Component } from "react";
-import {ListGroup,ListGroupItem} from 'reactstrap';
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
+  //We are create CategoryList Data
+  //State is like data
+  state = {
+    categories: [],
+  };
+
+  componentDidMount(){
+    this.getCategories();
+  }
+
+  getCategories = ()=>{
+    fetch("http://localhost:3000/categories")
+    .then(response=>response.json())
+    .then(data=>this.setState({categories:data}))
+  }
+
+
+
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
         <ListGroup>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem>Morbi leo risus</ListGroupItem>
-          <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          {this.state.categories.map(category=>(
+          <ListGroupItem 
+          onClick={()=>this.props.changeCategory(category)}
+           key={category.id}>{category.categoryName}
+           </ListGroupItem>
+          ))}
         </ListGroup>
+          {/* <h4>{this.props.currentCategory}</h4> */}
       </div>
     );
   }
--- FirstReactExample/src/ProductList.js
+++ FirstReactExample/src/ProductList.js
@@ -1,11 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Table } from "reactstrap";
 
 export default class ProductList extends Component {
-    render() {
-        return (
-            <div>
-                <h3>{this.props.info.title}</h3>
-            </div>
-        )
-    }
+  state = {
+    products: [
+      { productId: 1, productName: "MeyveSuyu" },
+      { productId: 2, productName: "Coca Cola" },
+    ],
+  };
+  render() {
+    return (
+      <div>
+        <h3>
+          {this.props.info.title}-{this.props.currentCategory}
+        </h3>
+        <Table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Product Name</th>
+              <th>Unit Price</th>
+              <th>Quantity Per Unit</th>
+              <th>Unit In Stock</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.products.map((product) => (
+              <tr key={product.id}>
+                <th scope="row">{product.id}</th>
+                <td>{product.productName}</td>
+                <td>{product.unitPrice}</td>
+                <td>{product.quantityPerUnit}</td>
+                <td>{product.unitsInStock}</td>
+              </tr>
+            ))}
+
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
