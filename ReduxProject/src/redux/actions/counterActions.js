fc9f87b2c82c0c2e98ef736c2eae511734cf59fd
Enes Kamis <eneskamis@yandex.com>
2020-11-20 21:45:16 +0300

    redux first project final
    redux first project final

--- ReduxProject/src/redux/actions/counterActions.js
+++ ReduxProject/src/redux/actions/counterActions.js
@@ -1,16 +1,20 @@
 import * as actionTypes from "./actionTypes"
 
-export const increaseCounter()=>({
+export const increaseCounter=()=>({
     type:actionTypes.INCREASE_COUNTER,
     payload:1
 })
 
-export const decreaseCounter()=>({
+export const decreaseCounter=()=>({
     type:actionTypes.DECREASE_COUNTER,
     payload:1
 })
 
-export const increaseByTwoCounter()=>({
+export const increaseByTwoCounter=()=>({
     type:actionTypes.INCREASE_BY_TWO_COUNTER,
+    payload:2
+})
+export const decreaseByTwoCounter=()=>({
+    type:actionTypes.DECREASE_BY_TWO_COUNTER,
     payload:2
 })
