303facd2dee189ec817acbe7064d8d79d5a29802
Enes Kamis <eneskamis@yandex.com>
2020-11-28 23:25:47 +0300

    error process
    error process

--- NorthWind-Redux/src/redux/actions/productAction.js
+++ NorthWind-Redux/src/redux/actions/productAction.js
@@ -1,11 +1,24 @@
+import { prettyDOM } from '@testing-library/react';
 import * as actionTypes from './actionTypes';
-
 export function getProductsSuccess(products){
     return {
         type: actionTypes.GET_PRODUCTS_SUCCESS, // CHANGE_CATEGORY
         payload: products
     }
 }
+
+export function createProductSuccess(product){
+    return{
+        type:actionTypes.CREATE_PRODUCT_SUCCESS,payload:product
+    }
+}
+
+export function updateProductSuccess(product){
+    return{
+        type:actionTypes.UPDATE_PRODUCT_SUCCESS,payload:product
+    }
+}
+
 
 export function getProducts(categoryId){
     return function(dispatch){
