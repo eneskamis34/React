57eff1dfad0e86e955f7e2c08cc6915e214beb01
= <eneskamis@yandex.com>
2020-12-04 23:26:35 +0300

    auth with google
    auth with google

--- bigReactProject/index.js
+++ bigReactProject/index.js
@@ -1,11 +1,32 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const key = require("./config/key");
+
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send({bye:'budy'});
-    
-})
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: key.googleClientID,
+      clientSecret: key.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
+
+
