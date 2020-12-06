b540d121b2e0d6330d5d0207d259318c4a428f9c
= <eneskamis@yandex.com>
2020-12-06 20:46:02 +0300

    model
    model

--- bigReactProject/index.js
+++ bigReactProject/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/key");
+require("./models/User");
+
 require("./services/passport");
 
 const app = express();
+
+mongoose.connect(keys.mongoURI);
+
 
 require("./routes/authRoutes")(app); 
 
