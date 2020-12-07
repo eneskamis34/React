33c55734fd6daa018cf7d6b01f54e6b06ea6db2e
= <eneskamis@yandex.com>
2020-12-07 23:04:01 +0300

    cookies
    cookies

--- bigReactProject/index.js
+++ bigReactProject/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/key");
 require("./models/User");
-
 require("./services/passport");
 
 const app = express();
 
 mongoose.connect(keys.mongoURI);
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys:[keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 require("./routes/authRoutes")(app); 
--- bigReactProject/services/passport.js
+++ bigReactProject/services/passport.js
@@ -5,6 +5,15 @@ const key = require("../config/key");
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user,done)=>{
+    done(null,user.id);
+});
+
+passport.deserializeUser((id, done)=>{
+    User.findById(id).then(user => {
+      done(null, user);
+    });
+});
 
 passport.use(
     new GoogleStrategy(
@@ -14,7 +23,19 @@ passport.use(
         callbackURL: "/auth/google/callback",
       },
       (accessToken,refreshToken,profile,done) => {
-          new User({ googleId: profile.id}).save();
+        User.findOne({googleId:profile.id})
+        .then(existingUser => {
+          if(existingUser) {
+            //we already have a record with the given profile ID
+            done(null, existingUser);
+          }
+          else {
+            // we dont have a user record.
+            new User({ googleId: profile.id})
+            .save()
+            .then(user => done(null,user));
+          }
+        });
       }
     )
   );
