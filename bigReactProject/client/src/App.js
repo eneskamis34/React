9b9e3e0ce6341c4dc30a4df45a8705562d7ae47c
= <eneskamis@yandex.com>
2020-12-10 22:23:12 +0300

    redux öncesi son çalışan kısım
    redux öncesi son çalışan kısım

--- bigReactProject/client/src/App.js
+++ bigReactProject/client/src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a href="/auth/google">Sign In With Google</a>
+
         <a
           className="App-link"
           href="https://reactjs.org"
