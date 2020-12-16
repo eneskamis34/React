33ee934601cec096568089f9713053e7311a3e21
= <eneskamis@yandex.com>
2020-12-16 23:21:11 +0300

    payment finish
    payment finish

---
+++ bigReactProject/config/keys.js
@@ -0,0 +1,10 @@
+//keys.js -figure out what set of credentials to return
+if(process.env.NODE_ENV === 'production')
+{
+    module.exports = require('./prod');
+}
+else 
+{
+    // we are in development- return the dev keys!!
+    module.exports = require('./dev');
+}
--- bigReactProject/services/passport.js
+++ bigReactProject/services/passport.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require('mongoose');
-const key = require("../config/key");
+const key = require("../config/keys");
 
 const User = mongoose.model('users');
 
