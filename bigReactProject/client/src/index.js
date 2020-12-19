f19eb8ec90be0c024b608ba9abc77d70e2888ca4
= <eneskamis@yandex.com>
2020-12-19 22:54:42 +0300

    send mail
    send mail

--- bigReactProject/client/src/index.js
+++ bigReactProject/client/src/index.js
@@ -1,16 +1,21 @@
-import materializeCSS from 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
- <Provider store={store}><App/></Provider>,
- document.querySelector('#root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
 );
